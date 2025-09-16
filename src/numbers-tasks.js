29ca4b8e3daa7904e4272ea8ee20e90b59d519a6
Denis Vasilenko <ghostinchip@gmail.com>
2025-09-16 21:37:34 +0300

    feat: Returns true is the number is prime; otherwise false is done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -199,8 +199,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const k = Math.trunc(Math.sqrt(n));
+  for (let i = 2; i <= k; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
